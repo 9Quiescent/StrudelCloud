8adce7fafa18af771dc972a1af32e69eae740e40
unknown <dkalongonda@gmail.com>
2025-11-26 18:25:36 +1030

    (Chore): Annotate StrudelPreset model for validation

--- StrudelWebApp/Models/StrudelPreset.cs
+++ StrudelWebApp/Models/StrudelPreset.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace StrudelWebApp.Models
 {
     public class StrudelPreset
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(200)]
         public string Name { get; set; } = string.Empty;
 
+        [Required] 
         public string RawCode { get; set; } = string.Empty;
 
+        [Required] 
         public string ControlsJson { get; set; } = string.Empty;
 
+        [Required]
         public DateTime CreatedAt { get; set; }
     }
 }
